d9b9eb3eb9aa618f0485182b75b508e6023c09fd
arbiekreme <tecsonronaldbo@gmail.com>
2020-05-27 13:36:43 +0800

    Added Files

---
+++ precache-manifest.82f83972bde750d6b5fd1b9df4f740b6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f273b63bb590a99f2e230fa804a0800d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f31f61608ba0d9fc7dc4",
+    "url": "/static/css/main.a2b7b0b6.chunk.css"
+  },
+  {
+    "revision": "9ce000ee2552a1823142",
+    "url": "/static/js/2.ba0ead68.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.ba0ead68.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f31f61608ba0d9fc7dc4",
+    "url": "/static/js/main.15a6d081.chunk.js"
+  },
+  {
+    "revision": "80e274ec0ec388e5e202",
+    "url": "/static/js/runtime-main.c8b434d9.js"
+  }
+]);
