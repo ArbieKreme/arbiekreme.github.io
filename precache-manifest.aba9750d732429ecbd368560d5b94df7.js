f106fe2ecc411ff28d4017bce93174a76768a63a
arbiekreme <tecsonronaldbo@gmail.com>
2020-05-27 15:17:15 +0800

    Commited another build

---
+++ precache-manifest.aba9750d732429ecbd368560d5b94df7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "18998785202fd384d80d24e65ff43432",
+    "url": "/index.html"
+  },
+  {
+    "revision": "54db8edc477eac4c5448",
+    "url": "/static/css/main.a2b7b0b6.chunk.css"
+  },
+  {
+    "revision": "9ce000ee2552a1823142",
+    "url": "/static/js/2.ba0ead68.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.ba0ead68.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "54db8edc477eac4c5448",
+    "url": "/static/js/main.d5c27c41.chunk.js"
+  },
+  {
+    "revision": "80e274ec0ec388e5e202",
+    "url": "/static/js/runtime-main.c8b434d9.js"
+  }
+]);
